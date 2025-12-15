36663ac8e042e8ac51a748e6ac05b1a70bbd4c07
Tarun palekar <tarunpalekar292@gmail.com>
2025-12-16 01:16:34 +0530

    setup agents

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
+
  
 const Page=async()=>{
   const session=await auth.api.getSession({
@@ -13,6 +14,7 @@ const Page=async()=>{
   }
   return(
     <>
+   
   {/* <homeView/> */}
   </>
 )
