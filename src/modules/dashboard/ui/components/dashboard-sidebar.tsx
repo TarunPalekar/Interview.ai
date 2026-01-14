cf3ed936407ef93cc5b37c77cb2f7f12afa422f7
TarunPalekar <167878468+TarunPalekar@users.noreply.github.com>
2026-01-14 21:36:54 +0530

    Update dashboard-sidebar.tsx

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -17,7 +17,7 @@ import {
 import {VideoIcon, BotIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+
 import DashboarduserButton from "./dashboard-user-button";
 
 
